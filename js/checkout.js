1c992ab24daaf4fd1bece2e5dd8999b3c9c2bbbf
Ttomaszito <ttomaszito@gmail.com>
2018-12-07 16:44:46 -0500

    ticket functionality refined

--- js/checkout.js
+++ js/checkout.js
@@ -11,15 +11,19 @@ $(document).ready(function () {
         paramArray.push(param)
     });
 
-    console.log(paramArray)
+    let concetDate = paramArray[0]
+    let seatingType = paramArray[2]
+    let ticketQuantity = paramArray[1]
+
+    console.log("paramArray", concetDate, seatingType, ticketQuantity)
 
     if (paramArray[1] > 1) {
-        $("#userPreferences").append("<h3> We have reserved " + paramArray[1]+ " seats in the " + paramArray[2]+ " section for the following show: </h3>");
-        $("#userPreferences").append("<h3>"+paramArray[0]+"</h3>");
+        $("#userPreferences").append("<h3> We have reserved " + paramArray[1] + " seats in the " + paramArray[2] + " section for the following show: </h3>");
+        $("#userPreferences").append("<h3>" + paramArray[0] + "</h3>");
     }
     else {
-        $("#userPreferences").append("<h3> We have reserved " + paramArray[1]+ " seat in the " + paramArray[2]+ " section for the following show: </h3>");
-        $("#userPreferences").append("<h3>"+paramArray[0]+"</h3>");
+        $("#userPreferences").append("<h3> We have reserved " + paramArray[1] + " seat in the " + paramArray[2] + " section for the following show: </h3>");
+        $("#userPreferences").append("<h3>" + paramArray[0] + "</h3>");
     }
 
     $("#purchaseBtn").click(function (event) {
@@ -33,6 +37,7 @@ $(document).ready(function () {
         let MailingState = $("#mailState").val();
         let MailingZipCode = $("#mailZipcode").val();
         let MailingPhoneNumber = $("#mailPhoneNumber").val();
+        let MailingEmail = $("#mailEmail").val()
 
         let BillingName = $("#billName").val();
         let BillingCity = $("#billCity").val();
@@ -41,15 +46,36 @@ $(document).ready(function () {
         let BillingState = $("#billState").val();
         let BillingZipCode = $("#billZipcode").val();
         let BillingPhoneNumber = $("#billPhoneNumber").val();
+        let BillingEmail = $("#billEmail").val()
 
         let CreditCardNumber = $("#ccNumber").val();
         let CreditCardExpiration = $("#ccExpiration").val();
         let CreditCardSecurityCode = $("#securityCode").val();
 
-        console.log("click ", MailingName, MailingCity, MailingAddress1, MailingAddress2, MailingState, MailingZipCode, MailingPhoneNumber,
-            BillingName, BillingCity, BillingAddress1, BillingAddress2, BillingState, BillingZipCode, BillingPhoneNumber, CreditCardNumber,
-            CreditCardExpiration, CreditCardSecurityCode)
+        let infoArray = [MailingName, MailingCity, MailingAddress1, MailingAddress2, MailingState, MailingZipCode, MailingPhoneNumber, MailingEmail,
+            BillingName, BillingCity, BillingAddress1, BillingAddress2, BillingState, BillingZipCode, BillingPhoneNumber, BillingEmail, CreditCardNumber,
+            CreditCardExpiration, CreditCardSecurityCode]
 
+        console.log(infoArray)
+
+        let errorTrigger = 0
+        console.log(errorTrigger)
+
+        infoArray.forEach(element => {
+
+            if (element.length == 0) {
+                errorTrigger = 1
+                return
+            }
+        });
+
+       
+        if(errorTrigger ==0){
+            window.location.href = "confirmation.html?param=" + concetDate + "&param2=" + ticketQuantity + "&param3=" + seatingType;
+        }
+        else{
+            alert("Make sure you fill out all required fields.")
+        }
 
     });
 
--- js/tickets.js
+++ js/tickets.js
@@ -12,7 +12,15 @@ $(document).ready(function () {
 
         console.log("click ", concetDate, ticketQuantity, seatingType)
 
-        window.location.href = "checkout.html?param=" + concetDate + "&param2=" + ticketQuantity + "&param3=" + seatingType;
+        if (concetDate != "Select shows" && ticketQuantity > 0 && seatingType != undefined) {
+            window.location.href = "checkout.html?param=" + concetDate + "&param2=" + ticketQuantity + "&param3=" + seatingType;
+        }
+
+        else {
+
+            alert("Make sure you have selected a show, number of tickets and seating.")
+
+        }
 
 
 
