2a6fe4e400f1e97d28542f638344cc364e25d4b6
Ttomaszito <ttomaszito@gmail.com>
2018-12-06 19:46:03 -0500

    fixed header and footer and styles every page

--- js/tickets.js
+++ js/tickets.js
@@ -4,11 +4,15 @@ $(document).ready(function () {
 
         event.preventDefault();
 
+        
+
         let concetDate = $("#inputGroupSelect03 option:selected").text();
         let ticketQuantity = $("#quantityInput").val();
-
         let seatingType = $(".radiobtn:checked").val()
+
         console.log("click ", concetDate, ticketQuantity,seatingType)
+
+        // I already made the form so the script gets all the values that you can use to render the next section
 
 
 
